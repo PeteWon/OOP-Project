77a8c002b3ecc865902814224171b5d97acc523d
Sonnetica <chrischua02@gmail.com>
2025-02-07 18:17:30 +0800

    added jjiii

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/Circle.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/Circle.java
@@ -1,5 +1,6 @@
 package io.github.some_example_name.lwjgl3;
 //kk part 2
+//jjii
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.Gdx;
