a78dcf64e76f8271a87296f593ffdec8e9de871a
PeteWon <2401243@sit.singaporetech.edu.sg>
2025-02-14 16:20:28 +0800

    comment stuff at game master

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/GameMaster.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/GameMaster.java
@@ -1,15 +1,15 @@
 package io.github.some_example_name.lwjgl3;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
+// import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-import io.github.some_example_name.lwjgl3.Entity.Entity;
-import io.github.some_example_name.lwjgl3.Entity.EntityManager;
+// import io.github.some_example_name.lwjgl3.Entity.Entity;
+// import io.github.some_example_name.lwjgl3.Entity.EntityManager;
 
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.Color;
+// import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+// import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+// import com.badlogic.gdx.graphics.Color;
 
 	
 public class GameMaster extends ApplicationAdapter {
