5ec9722e39e09ff6942e041c232f20d419a8cda9
nafisuu <muhdnafis02@gmail.com>
2025-02-14 17:08:02 +0800

    updated version for gamemaster.java

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/GameMaster.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/GameMaster.java
@@ -1,41 +1,34 @@
 package io.github.some_example_name.lwjgl3;
 
 import com.badlogic.gdx.ApplicationAdapter;
-// import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-// import io.github.some_example_name.lwjgl3.Entity.Entity;
-// import io.github.some_example_name.lwjgl3.Entity.EntityManager;
+import io.github.some_example_name.lwjgl3.Collision.CollisionManager;
+import io.github.some_example_name.lwjgl3.Entity.EntityManager;
+import io.github.some_example_name.lwjgl3.IO.IOManager;
+import io.github.some_example_name.lwjgl3.Scene.SceneManager;
 
-// import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-// import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-// import com.badlogic.gdx.graphics.Color;
-
-	
 public class GameMaster extends ApplicationAdapter {
+    private EntityManager entityManager;
+    private CollisionManager collisionManager;
+    private IOManager ioManager;
+    private SceneManager sceneManager;
 
+    @Override
+    public void create() {
+        entityManager = new EntityManager();
+        collisionManager = new CollisionManager();
+        ioManager = new IOManager();
+        sceneManager = new SceneManager();
+    }
 
-	@Override
-	public void create() {
-
-		// batch = new SpriteBatch();
-		// shape = new ShapeRenderer();
-        // entityManager = new EntityManager();
-		}
-	
-	@Override
-	public void render() {
-
-		ScreenUtils.clear(0, 0, 0.2f, 1);
+    @Override
+    public void render() {
+        ScreenUtils.clear(0, 0, 0.2f, 1);
 		
-	}
-	
+    }
+
     @Override
     public void dispose() {
-
-
     }
 }
-
-
-
