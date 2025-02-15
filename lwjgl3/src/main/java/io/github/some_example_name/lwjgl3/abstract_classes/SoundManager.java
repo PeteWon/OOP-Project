83b6883d0abae9806edaf2d004c81da717f8a920
eeugenewyj <eugene.ong2002@gmail.com>
2025-02-15 11:59:29 +0800

    sorted out errors

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/abstract_classes/SoundManager.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/abstract_classes/SoundManager.java
@@ -3,12 +3,12 @@ package io.github.some_example_name.lwjgl3.abstract_classes;
 public abstract class SoundManager {
     
     //get-set for volume
-    public abstract void setVolume(boolean loop);
+    public abstract void setVolume(float volume);
     public abstract float getVolume();
 
     //get-set for loop
-    public abstract void setLoop(float volume);
-    public abstract boolean getLoop();
+    public abstract void setLoop();
+    public abstract boolean getLoop(boolean loop);
 
     //audio controls
     public abstract void playMusic();
