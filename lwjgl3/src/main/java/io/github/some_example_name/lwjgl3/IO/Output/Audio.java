54d8653aaafae85ab07433fff88951c7a407b30c
eeugenewyj <eugene.ong2002@gmail.com>
2025-02-15 11:58:35 +0800

    added audio file, functions

---
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/IO/Output/Audio.java
@@ -0,0 +1,76 @@
+package io.github.some_example_name.lwjgl3.IO.Output;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+
+import io.github.some_example_name.lwjgl3.abstract_classes.SoundManager;
+
+public class Audio extends SoundManager{
+
+    private Music gameMusic;
+    private String music_name;
+    private float volume;
+    private boolean loop;
+
+    public Audio(){ // plays default music
+        this.music_name = "Music/MainScreenMusic.mp3";
+        this.volume = .1f;
+        this.loop = true;
+        this.gameMusic = Gdx.audio.newMusic(Gdx.files.internal(this.music_name));
+        this.gameMusic.setVolume(this.volume);
+        this.gameMusic.setLooping(this.loop);
+    }
+
+    public Audio(String music_name, float volume, boolean loop){ // plays selected music
+        this.music_name = music_name;
+        this.volume = volume;
+        this.loop = loop;
+        this.gameMusic = Gdx.audio.newMusic(Gdx.files.internal(this.music_name));
+        this.gameMusic.setVolume(this.volume);
+        this.gameMusic.setLooping(this.loop);
+    }
+
+    public void setMusicName(String music_name){
+        this.music_name = music_name;
+        this.gameMusic = Gdx.audio.newMusic(Gdx.files.internal(this.music_name));
+    }
+
+    public String getMusicName(){
+        return this.music_name;
+    }
+
+    public void setVolume(float volume){
+        this.volume = volume;
+        this.gameMusic.setVolume(this.volume);
+    }
+
+    public float getVolume(){
+        return this.volume;
+    }
+
+    public void setLoop(boolean loop){
+        this.loop = loop;
+        this.gameMusic.setLooping(this.loop);
+    }
+
+    public boolean getLoop(){
+        return this.loop;
+    }
+
+    public void playMusic(){
+        this.gameMusic.play();
+    }
+
+    public void stopMusic(){
+        this.gameMusic.stop();
+    }
+
+    public boolean isPlayingMusic(){
+        return this.gameMusic.isPlaying();
+    }
+
+    public void dispose(){
+        this.gameMusic.dispose();
+    }
+
+}
--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/abstract_classes/SoundManager.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/abstract_classes/SoundManager.java
@@ -2,31 +2,20 @@ package io.github.some_example_name.lwjgl3.abstract_classes;
 
 public abstract class SoundManager {
     
-    //sound effects
-    public abstract void loadSound(String soundId, String filepath);
-    public abstract void playSound(String soundId, boolean loop);
-    public abstract void stopSound(String soundId);
-    public abstract void pauseSound(String soundId);
-    public abstract void resumeSound(String soundId);
+    //get-set for volume
+    public abstract void setVolume(boolean loop);
+    public abstract float getVolume();
 
-    //background music
-    public abstract void loadMusic(String filepath);
-    public abstract void playMusic(boolean loop);
+    //get-set for loop
+    public abstract void setLoop(float volume);
+    public abstract boolean getLoop();
+
+    //audio controls
+    public abstract void playMusic();
     public abstract void stopMusic();
-    public abstract void pauseMusic();
-    public abstract void resumeMusic();
-
-    //volume control
-    public abstract void setSoundVolume(float volume);
-    public abstract void setMusicVolume(float volume);
-
-    //mute control
-    public abstract void muteSounds();
-    public abstract void unmuteSounds();
-    public abstract void muteMusic();
-    public abstract void unmuteMusic();
+    public abstract boolean isPlayingMusic();
 
     //cleanup
-    public abstract void cleanup();
+    public abstract void dispose();
 
 }
