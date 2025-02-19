cd4d68d2ea5021f86d7e9f5022c822ad0333258c
eeugenewyj <eugene.ong2002@gmail.com>
2025-02-20 02:48:05 +0800

    hash map, for soundfx mapping

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/IO/Output/Audio.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/IO/Output/Audio.java
@@ -2,6 +2,10 @@ package io.github.some_example_name.lwjgl3.IO.Output;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import io.github.some_example_name.lwjgl3.IO.OutputManager;
 import io.github.some_example_name.lwjgl3.abstract_classes.AudioHandler;
@@ -12,6 +16,8 @@ public class Audio extends AudioHandler {
 
     private Music gameMusic;
     private String music_name;
+
+    private Map<String, Sound> soundEffects;
 
     private float volume;
     private boolean loop;
@@ -27,6 +33,9 @@ public class Audio extends AudioHandler {
         this.gameMusic = Gdx.audio.newMusic(Gdx.files.internal(this.music_name));
         this.gameMusic.setVolume(this.volume);
         this.gameMusic.setLooping(this.loop);
+
+        this.soundEffects = new HashMap<>();
+
         System.out.println("Music started: " + this.music_name);
     }
 
@@ -114,7 +123,51 @@ public class Audio extends AudioHandler {
         return this.gameMusic.isPlaying();
     }
 
+    public void loadSoundEffect(String key, String filePath) {
+        if (!soundEffects.containsKey(key)) {
+            Sound sound = Gdx.audio.newSound(Gdx.files.internal(filePath));
+            soundEffects.put(key, sound);
+            System.out.println("Sound effect loaded: " + filePath);
+        }
+    }
+
+    // Play a sound effect
+    public void playSoundEffect(String key) {
+        Sound sound = soundEffects.get(key);
+        if (sound != null) {
+            sound.play();
+            System.out.println("Playing sound effect: " + key);
+        } else {
+            System.out.println("Sound effect not found: " + key);
+        }
+    }
+
+    // Stop a sound effect (only if looping)
+    public void stopSoundEffect(String key) {
+        Sound sound = soundEffects.get(key);
+        if (sound != null) {
+            sound.stop();
+            System.out.println("Stopped sound effect: " + key);
+        }
+    }
+
+    // Set volume for a sound effect
+    public void setSoundEffectVolume(String key, float volume) {
+        Sound sound = soundEffects.get(key);
+        if (sound != null) {
+            sound.play(volume); // Play at given volume (LibGDX doesn't support direct volume control for Sound)
+            System.out.println("Set sound effect volume: " + key + " to " + volume);
+        }
+    }
+
+    // Dispose all sounds
     public void dispose() {
         this.gameMusic.dispose();
+        for (Sound sound : soundEffects.values()) {
+            sound.dispose();
+        }
+        soundEffects.clear();
+        System.out.println("Audio resources disposed.");
     }
+
 }
